657a86d54aeb255689150e0297910baa5c99691f
avenable <avenable11@ivytech.edu>
2020-02-12 20:17:05 -0500

    State after 2/12/2020 lecture

--- app/src/main/java/edu/ivytech/newtipcalculator/TipCalculatorActivity.java
+++ app/src/main/java/edu/ivytech/newtipcalculator/TipCalculatorActivity.java
@@ -2,14 +2,20 @@ package edu.ivytech.newtipcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -30,6 +36,15 @@ public class TipCalculatorActivity extends AppCompatActivity {
     private SharedPreferences savedValues;
     private String billAmountString;
     private Spinner splitSpinner;
+    private int split = 1;
+    private SeekBar tipSeek;
+    private RadioGroup roundingRadioGroup;
+    private int round = 0;
+
+    private final int ROUND_NONE = 0;
+    private final int ROUND_TIP = 1;
+    private final int ROUND_TOTAL = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +61,9 @@ public class TipCalculatorActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
                 if(i == EditorInfo.IME_ACTION_DONE || i == EditorInfo.IME_ACTION_UNSPECIFIED
                 || i == EditorInfo.IME_ACTION_NEXT) {
+                    InputMethodManager imm =
+                            (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(billAmountEditText.getWindowToken(),0);
                     calculateAndDisplay();
                 }
                 return false;
@@ -60,6 +78,62 @@ public class TipCalculatorActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         splitSpinner.setAdapter(adapter);
         splitSpinner.setSelection(0);
+
+        splitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                split = position + 1;
+                calculateAndDisplay();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        tipSeek = findViewById(R.id.tipSeek);
+        tipSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                percentEditText.setText(String.format("%d", i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                tipPercent = (float)progress/100.0f;
+                calculateAndDisplay();
+            }
+        });
+
+        roundingRadioGroup = findViewById(R.id.roundingRadioGroup);
+        roundingRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                Log.d("Tip Calculator","Entered onCheckedChanged");
+                switch(i) {
+                    case R.id.noRoundingRadioButton:
+                        round = ROUND_NONE;
+                        Log.d("Tip Calculator", "no round");
+                        break;
+                    case R.id.roundTipRadioButton:
+                        round = ROUND_TIP;
+                        Log.d("Tip Calculator", "round tip");
+                        break;
+                    case R.id.roundTotalRadioButton:
+                        round = ROUND_TOTAL;
+                        Log.d("Tip Calculator", "round total");
+                        break;
+                }
+                calculateAndDisplay();
+
+            }
+        });
     }
 
     @Override
@@ -79,6 +153,9 @@ public class TipCalculatorActivity extends AppCompatActivity {
         tipPercent = savedValues.getFloat("tipPercent",0.15f);
 
         billAmountEditText.setText(billAmountString);
+        int progress = (int)(tipPercent * 100);
+        tipSeek.setProgress(progress);
+
         calculateAndDisplay();
     }
 
@@ -96,6 +173,18 @@ public class TipCalculatorActivity extends AppCompatActivity {
         float tipAmount = billAmount * tipPercent;
         float totalAmount = billAmount + tipAmount;
 
+
+        switch(round) {
+            case ROUND_NONE:
+                break;
+            case ROUND_TIP:
+                tipAmount = (float)Math.ceil(tipAmount);
+                break;
+            case ROUND_TOTAL:
+               totalAmount = (float)Math.ceil(totalAmount);
+                break;
+        }
+        totalAmount = totalAmount / split;
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         NumberFormat percent = NumberFormat.getPercentInstance();
         DecimalFormatSymbols symbols = new DecimalFormatSymbols();
